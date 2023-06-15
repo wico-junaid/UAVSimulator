b71f0d65c3e0c4646fa062f0ac026dabca73ff20
wico-junaid <junaid@wingcopter.com>
2023-06-15 16:54:20 +0200

    Documentation and test cases added

--- src/test/java/com/wingcopter/UAVSimulator/service/GNSSDataProducerServiceTest.java
+++ src/test/java/com/wingcopter/UAVSimulator/service/GNSSDataProducerServiceTest.java
@@ -4,12 +4,13 @@ import com.wingcopter.UAVSimulator.model.GnssData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GNSSDataProducerServiceTest {
 
-    static GnssData gnssData = new GnssData();
+    GnssData gnssData = new GnssData();
 
     @BeforeEach
     void setUp() {
@@ -22,20 +23,8 @@ class GNSSDataProducerServiceTest {
 
     @Test
     void sendGNSSDataNoArgumentsTest() {
-        assertThrows(IllegalArgumentException.class,
-                GNSSDataProducerService::sendGNSSData);
-    }
-
-    @Test
-    void sendGnssDataWithArgumentsTest(){
-        gnssData.setStartLatitude(37.7749);
-        gnssData.setStartLongitude(-122.4194);
-        gnssData.setEndLatitude(34.0522);
-        gnssData.setEndLatitude(-118.2437);
-
         GNSSDataProducerService gnssDataProducerService = new GNSSDataProducerService(gnssData);
-        gnssDataProducerService.sendGNSSData();
-
-        assertNull(gnssDataProducerService);
+        assertThrows(IllegalArgumentException.class,
+                gnssDataProducerService::sendGNSSData);
     }
 }
